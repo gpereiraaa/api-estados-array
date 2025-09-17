ab39cad6bf9c6f40ec4c0000894411196478654f
GUSTAVO PEREIRA LEITE DA SILVA <25132694@CFP127ALUNO.LOCAL>
2025-09-17 16:58:57 -0300

    Faltando apenas tres funcoes e fazer os endpoints

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+// Import do arquivo de funções 
+const dados = require('./modulo/funcoes.js')
+
 // Define a porta padrão da API, se for em um servidor de nuvem não temos acesso a porta 
 //                      em execução local podemos definir uma porta livre
 const  PORT = process.PORT || 8080
@@ -24,14 +27,46 @@ const  PORT = process.PORT || 8080
 // Instacia na classe do express
 const app = express()
 
+// Configurações do CORS
 app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header('Access-Control-Allow-Methods', 'GET')
+    response.header('Access-Control-Allow-Origin', '*') // IP de origem 
+    response.header('Access-Control-Allow-Methods', 'GET') // Metodos (Verbos) do protocolo HTTP
 
     app.use(cors())
-    next()
+    next() // Proximo 
 })
 
-app.get('/v1/estados', function(request, response){
+// Request  --> Recebe os dados da API
+// Response --> Envia os dados na API
 
+// EndPoints
+app.get('/v1/estados', function(request, response){
+    let estados = dados.getAllEstados()
+
+    response.status(estados.statuscode)
+    response.json(estados)
+})
+
+app.get('/v1/estado/:uf', function(request, response){
+    let sigla = request.params.uf
+
+    console.log(sigla)
+})
+/*
+Exemplo de query e params:
+Query -> para passar filtros
+e Params para passar ID
+app.get('/v1/regiao/:id', function(request, response){
+    let regiaoEstados = request.query.regiao
+    let sigla = request.query.uf
+    let id = request.params.id
+
+    console.log(id)
+    console.log(regiaoEstados)
+    console.log(sigla)
+})*/
+
+// Start da API
+app.listen(PORT, function(){
+    console.log('API aguardando requisições ...')
 })
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -11,6 +11,7 @@ const MESSAGE_ERROR = {status: false, statuscode: 500, development: 'Gustavo Per
 
 // Retorna a lista de estados 
 const getAllEstados = function() {
+    // Padrão do JSON que será o retorno da função
     let message = {status: true, statuscode: 200, development: 'Gustavo Pereira', uf: []}
 
     dados.listaDeEstados.estados.forEach(function(item){
@@ -21,7 +22,8 @@ const getAllEstados = function() {
     message.quantidade = message.uf.length
 
     // Apaga um elemento existente no JSON
-    delete message.status
+    //delete message.status
+
     if (message.uf.length > 0) 
         return message // Resultado verdadeiro da API 200
      else
@@ -30,17 +32,61 @@ const getAllEstados = function() {
 }
 
 // Retorna dados do estado filtrando pela sigla
-const getEstadoBySigla = function(sigla){
+const getEstadoBySigla = function(uf){
+    let message = {status: true, statuscode: 200, development: 'Gustavo Pereira', uf: [], descricao: [],capital: [], regiao: [] }
+
+    dados.listaDeEstados.estados.forEach(item => {
+        if (item.sigla === uf) {
+            message.uf.push(item.sigla)
+            message.descricao.push(item.nome)
+            message.capital.push(item.capital)
+            message.regiao.push(item.regiao)
+        }
+    })
+
+    if(uf === message.uf)
+        return message
+    else
+        return MESSAGE_ERROR
 
 }
 
 // Retorna a capital do estado filtrando pela sigla
-const getCapitalBySigla = function(sigla){
+const getCapitalBySigla = function(uf){
+    let message = {status: true, statuscode: 200, development: 'Gustavo Pereira', uf: [], descricao: [],capital: [] }
+
+    dados.listaDeEstados.estados.forEach(item => {
+        if (item.sigla === uf) {
+            message.uf.push(item.sigla)
+            message.descricao.push(item.nome)
+            message.capital.push(item.capital)
+        }
+    })
+
+    if(uf === message.uf)
+        return message
+    else
+        return MESSAGE_ERROR
 
 }
 
 // Retorna a lista de estados filtrando pela região
 const getEstadosByRegiao = function(regiao){
+    let message = {status: true, statuscode: 200, development: 'Gustavo Pereira', regiao: '', estados: [] }
+
+    dados.listaDeEstados.estados.forEach(item => {
+        if (item.regiao === regiao) {
+            message.regiao = item.regiao
+            message.estados.uf.push(item.sigla)
+            message.estados.descricao.push(item.nome)
+            //message.estados.uf = item.sigla
+            //message.estados.descricao = item.nome
+        }
+    })
+    if(regiao === message.regiao)
+        return message
+    else
+        MESSAGE_ERROR
 
 }
 
@@ -54,7 +100,10 @@ const getCidadesBySigla = function(sigla) {
 
 }
 
+console.log(getEstadosByRegiao('Sudeste'))
 
 module.exports = {
-    getAllEstados
+    getAllEstados,
+    getEstadoBySigla,
+    getCapitalBySigla
 }
